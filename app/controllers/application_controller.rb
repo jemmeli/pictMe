eb1c436b41f4a889492a42e5a24004736ee37735
najm <jemmeli84@gmail.com>
2020-03-01 23:58:19 +0100

    add style to devise view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,19 @@
 class ApplicationController < ActionController::Base
+
+  layout :layout_login
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   add_flash_types :success, :warning, :danger, :info
+
+
+  private
+  #https://github.com/heartcombo/devise/wiki/How-To:-Create-custom-layouts
+  def layout_login
+    if devise_controller?
+      "login"
+    end
+  end
 
 end
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,6 +6,7 @@ Rails.application.config.assets.version = '1.1'
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 Rails.application.config.assets.precompile += %w( diploma.css )
+Rails.application.config.assets.precompile += %w( login.css )
 #Rails.application.config.assets.precompile += %w( diploma_form.sass )
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
