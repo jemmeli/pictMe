fd61aac0cae9fa1ae9cd681eeab635c2ec86f63b
najm <jemmeli84@gmail.com>
2020-07-27 15:07:56 +0100

    redis setup 1

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,7 @@
 if Rails.env.staging? || Rails.env.production?
-  uri = URI.parse(ENV["REDIS_URL"])
+  #uri = URI.parse(ENV["REDIS_URL"])
   #uri = "http://127.0.0.1:6379/"
-  $redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
-  $redis.ping
+  #$redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
+  $redis = Redis.new(url: ENV["REDIS_URL"])
+  #$redis.ping
 end
