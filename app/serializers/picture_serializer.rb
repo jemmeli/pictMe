1e2f762182ac8a43353df99692d42bf1f6f28952
najm <jemmeli84@gmail.com>
2020-12-13 17:27:47 +0100

    correct total number photos

--- app/serializers/picture_serializer.rb
+++ app/serializers/picture_serializer.rb
@@ -2,9 +2,7 @@ class PictureSerializer < ActiveModel::Serializer
   attributes :id, :image_data,:edition_id, :bib, :created_at, :updated_at, :owner_of_picture
 
   def owner_of_picture
-    #owner_of_picture = Contact.where(dossard: object.bib, edition_id: params[:id])
-    owner_of_picture = Contact.where("dossard = ? AND edition_id = ?", object.bib, params[:edition_id])
-
+    owner_of_picture = Contact.where(dossard: object.bib)
     return owner_of_picture
   end
 
